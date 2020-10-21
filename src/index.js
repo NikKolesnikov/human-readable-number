c048f7f5ee7bebbb5a42681dbb1e23d99bebcb0a
NikKolesnikov <nikkolesnikov94@gmail.com>
2020-10-21 18:40:52 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
-}
+    const firstNumber = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const secondNumber = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let result = '';
+    number = String(number);
+
+    if (number < 20) {
+        result += firstNumber[number];
+    } else if (number >= 20 && number < 100) {
+        if (number[1] === '0') {
+            result += secondNumber[(number[0]) - 2];
+        } else {
+            result += secondNumber[(number[0]) - 2] + ' ' + firstNumber[(number[1])];
+        };
+    } else if (number >= 100 && number < 1000) {
+        if (number.slice(1) < 20) {
+
+            if (number[1] === '0' && number[2] === '0') {
+                result += firstNumber[(number[0])] + ' hundred';
+            } else if (number[1] === '0') {
+                result += firstNumber[(number[0])] + ' hundred ' + firstNumber[(number[2])];
+            } else {
+                result += firstNumber[(number[0])] + ' hundred ' + firstNumber[number.slice(1)];
+            };
+
+        } else if (number[2] === '0') {
+            result += firstNumber[(number[0])] + ' hundred ' + secondNumber[(number[1]) - 2];
+        } else {
+            result += firstNumber[(number[0])] + ' hundred ' + secondNumber[(number[1]) - 2] + ' ' + firstNumber[(number[2])];
+        };
+    };
+
+    return result;
+};
